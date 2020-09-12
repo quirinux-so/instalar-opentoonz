af4c8265a7b6b0e4786a393b2e5875954d5228cd
Quirinux GNU/Linux <49002514+quirinux-so@users.noreply.github.com>
2020-09-12 18:50:53 +0200

    English version

--- install-opentoonz.sh
+++ install-opentoonz.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# File:	  install-opentoonz.sh
-# Author:	Charlie Martínez® <cmartinez@quirinux.org>
-# License:	https://www.gnu.org/licenses/gpl-3.0.txt
-# Description:	Download OpenToonz source code, compile and install it.
-# Version:	1.00
+# File:     install-opentoonz.sh
+# Author:   Charlie Martínez® <cmartinez@quirinux.org>
+# License:    https://www.gnu.org/licenses/gpl-3.0.txt
+# Description:    Download OpenToonz source code, compile and install it.
+# Version:    1.00
 
 clear
 

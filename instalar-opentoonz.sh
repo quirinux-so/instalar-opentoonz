95efc12b3de1fa45deacea44523714edd23549e2
Quirinux GNU/Linux <49002514+quirinux-so@users.noreply.github.com>
2020-09-12 19:07:35 +0200

    English version

--- instalar-opentoonz.sh
+++ instalar-opentoonz.sh
@@ -3,21 +3,25 @@
 # Nombre:	instalar-opentoonz.sh
 # Autor:	Charlie Martínez® <cmartinez@quirinux.org>
 # Licencia:	https://www.gnu.org/licenses/gpl-3.0.txt
-# Descripción:	Descarga código fuente de OpenToonz, lo compila y lo instala.
+# Descripción:	Descarga el código fuente de la versión más nueva de OpenToonz, lo compila y lo instala. 
 # Versión:	1.00
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+${bold}
+${normal}
 
 clear
 
-echo "
+echo "--------------------------------------------------------------
+QUIRINUX PRO: COMPILAR E INSTALAR OPENTOONZ
+--------------------------------------------------------------
   ___                 _____                     
  / _ \ _ __   ___ _ _|_   _|__   ___  _ __  ____
 | | | | '_ \ / _ \ '_ \| |/ _ \ / _ \| '_ \|_  /
 | |_| | |_) |  __/ | | | | (_) | (_) | | | |/ / 
  \___/| .__/ \___|_| |_|_|\___/ \___/|_| |_/___|
       |_|
---------------------------------------------------------------
-QUIRINUX PRO: COMPILAR E INSTALAR OPENTOONZ
---------------------------------------------------------------
 
 Descargar código fuente e instalar compilando desde el código
 la versión más nueva del programa de animación profesional 
@@ -37,11 +41,23 @@ case $opc in
 
 "1") 
 
-# COMPILANDO OPENTOONZ DESDE SU CÓDIGO FUENTE
+clear
+
+# Instala dependencias del instalador
+
+sudo apt-get update -y
+for paquetes_wget in wget git software-properties-common; do sudo apt-get install -y $paquetes_wget; done
+sudo apt-get install -f -y
+sudo apt-get autoremove --purge -y
+
+# Instala dependencias de OpenToonz
 
 sudo apt-get update -y
 for paquetes_opentoonz in build-essential git cmake pkg-config libboost-all-dev qt5-default qtbase5-dev libqt5svg5-dev qtscript5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev qtmultimedia5-dev libsuperlu-dev liblz4-dev libusb-1.0-0-dev liblzo2-dev libpng-dev libjpeg-dev libglew-dev freeglut3-dev libfreetype6-dev libjson-c-dev qtwayland5 libqt5multimedia5-plugins; do sudo apt-get install -y $paquetes_opentoonz; done
 for paquetes_opentoonz2 in libmypaint-dev; do sudo apt-get install -y $paquetes_opentoonz2; done
+
+# Descarga y compila el código fuente de OpenToonz
+
 mkdir /opt/tmp
 cd /opt/tmp
 git clone https://github.com/opentoonz/opentoonz
@@ -73,10 +89,48 @@ sudo make install
 sudo apt-get install -f -y
 sudo apt-get autoremove --purge -y
 
-# DESCARGANDO Y COPIANDO EL ÍCONO DEL MENÚ DE INICIO
+# Descarga y copia en ícono del menú de inicio
 
 sudo wget  --no-check-certificate 'http://my.opendesktop.org/s/FZz85jagrQLCjjB/download' -O /opt/opentoonz/opentoonz-icon.tar
 sudo tar -xf /opt/opentoonz/opentoonz-icon.tar -C /
+
+# Creando comando de inicio
+
+sudo chmod -R 775 /opt/opentoonz
+sudo chown -R $USER /opt/opentoonz
+
+FILE="/usr/local/bin/imagine2"
+
+if [ -f "$FILE" ]; then
+
+sudo rm /usr/local/bin/opentoonz
+cd /usr/local/bin
+sudo ln -s /opt/opentoonz/bin/opentoonz
+sudo chmod -R 4755 /usr/local/bin/opentoonz
+
+else
+
+cd /usr/local/bin
+sudo ln -s /opt/opentoonz/bin/opentoonz
+sudo chmod -R 4755 /usr/local/bin/opentoonz
+
+fi
+
+
+clear
+
+clear
+
+echo "--------------------------------------------------------------
+INSTALLATION FINISHED SUCCESSFUL
+--------------------------------------------------------------
+ 
+Congratulations! OpenToonz is already on your system and you can
+open it by going to ${bold}Applications> Graphics> OpenToonz${normal} or from
+the terminal with the command ${bold}opentoonz.${normal}
+
+
+"
 
 ;;
 

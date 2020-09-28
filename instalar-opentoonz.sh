9cd11c312512bc7049f7c5dcf5c0f4a7abf56619
Quirinux GNU/Linux <49002514+quirinux-so@users.noreply.github.com>
2020-09-28 02:31:04 +0200

    Update instalar-opentoonz.sh
    Se agrega opcion para instalar testing nigthly build o estable 1.4

--- instalar-opentoonz.sh
+++ instalar-opentoonz.sh
@@ -22,17 +22,15 @@ QUIRINUX PRO: COMPILAR E INSTALAR OPENTOONZ
 | |_| | |_) |  __/ | | | | (_) | (_) | | | |/ / 
  \___/| .__/ \___|_| |_|_|\___/ \___/|_| |_/___|
       |_|
-
 Descargar código fuente e instalar compilando desde el código
 la versión más nueva del programa de animación profesional 
 OpenToonz con el que puedes reemplazar a Toon Boom Harmony.
-
 Compatible con Debian Buster, Devuan Beowulf y Ubuntu 20.4
 
-1 Descargar, compilar e instalar OpenToonz
+
+1 Descargar, compilar e instalar OpenToonz 1.4 (estable)
+2 Descargar, compilar e instalar OpenToonz nightly build (testing)
 0 Salir
-
-
 
 "
 
@@ -133,11 +131,93 @@ echo "--------------------------------------------------------------
 INSTALACIÓN FINALIZADA CON ÉXITO
 --------------------------------------------------------------
  
-¡Felicidades! OpenToonz ya está en tu sistema y puedes
+¡Felicidades! OpenToonz Estable 1.4 ya está en tu sistema y puedes
 abrirlo yendo a ${bold}Aplicaciones > Gráficos > OpenToonz${normal} o desde 
 la terminal con el comando ${bold}opentoonz${normal}
+"
 
+exit 0
 
+;;
+
+"2")
+
+# COMPILANDO OPENTOONZ DESDE SU CÓDIGO FUENTE
+
+sudo apt-get update -y
+for paquetes_opentoonz in build-essential git cmake pkg-config libboost-all-dev qt5-default qtbase5-dev libqt5svg5-dev qtscript5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev qtmultimedia5-dev libsuperlu-dev liblz4-dev libusb-1.0-0-dev liblzo2-dev libpng-dev libjpeg-dev libglew-dev freeglut3-dev libfreetype6-dev libjson-c-dev qtwayland5 libqt5multimedia5-plugins; do sudo apt-get install -y $paquetes_opentoonz; done
+for paquetes_opentoonz2 in libmypaint-dev; do sudo apt-get install -y $paquetes_opentoonz2; done
+mkdir /opt/tmp
+cd /opt/tmp
+git clone https://github.com/opentoonz/opentoonz
+mkdir -p $HOME/.config/OpenToonz
+cp -r opentoonz/stuff $HOME/.config/OpenToonz/
+cat << EOF > $HOME/.config/OpenToonz/SystemVar.ini
+[General]
+OPENTOONZROOT="$HOME/.config/OpenToonz/stuff"
+OpenToonzPROFILES="$HOME/.config/OpenToonz/stuff/profiles"
+TOONZCACHEROOT="$HOME/.config/OpenToonz/stuff/cache"
+TOONZCONFIG="$HOME/.config/OpenToonz/stuff/config"
+TOONZFXPRESETS="$HOME/.config/OpenToonz/stuff/fxs"
+TOONZLIBRARY="$HOME/.config/OpenToonz/stuff/library"
+TOONZPROFILES="$HOME/.config/OpenToonz/stuff/profiles"
+TOONZPROJECTS="$HOME/.config/OpenToonz/stuff/projects"
+TOONZROOT="$HOME/.config/OpenToonz/stuff"
+TOONZSTUDIOPALETTE="$HOME/.config/OpenToonz/stuff/studiopalette"
+EOF
+cd /opt/tmp/opentoonz/thirdparty/tiff-4.0.3
+./configure --with-pic --disable-jbig
+make -j$(nproc)
+cd ../../
+cd /opt/tmp/opentoonz/toonz
+mkdir build
+cd build
+cmake ../sources
+make -j$(nproc)
+sudo make install 
+sudo apt-get install -f -y
+sudo apt-get autoremove --purge -y
+
+# Descarga y copia en ícono del menú de inicio de OpenToonz
+
+sudo mv /opt/opentoonz/bin/opentoonz /opt/opentoonz/bin/opentoonz2
+sudo wget  --no-check-certificate 'http://my.opendesktop.org/s/FZz85jagrQLCjjB/download' -O /opt/tmp/opentoonz/opentoonz-icon.tar
+sudo mv /opt/tmp/opentoonz /opt/tmp/opentoonz-tmp
+sudo tar -xvf /opt/tmp/opentoonz-tmp/opentoonz-icon.tar -C /
+
+# Creando comando de inicio de OpenToonz
+
+sudo chmod -R 775 /opt/opentoonz
+sudo chown -R $USER /opt/opentoonz
+
+FILE="/usr/local/bin/opentoonz"
+
+if [ -f "$FILE" ]; then
+
+sudo rm /usr/local/bin/opentoonz
+mv /opt/tmp/opentoonz /opt/opentoonz/bin/opentoonz
+cd /usr/local/bin
+sudo ln -s /opt/opentoonz/bin/opentoonz
+sudo chmod 777 /usr/local/bin/opentoonz
+
+else
+
+mv /opt/tmp/opentoonz /opt/opentoonz/bin/opentoonz
+cd /usr/local/bin
+sudo ln -s /opt/opentoonz/bin/opentoonz
+sudo chmod 777 /usr/local/bin/opentoonz
+
+fi
+
+clear
+
+echo "--------------------------------------------------------------
+INSTALACIÓN FINALIZADA CON ÉXITO
+--------------------------------------------------------------
+ 
+¡Felicidades! OpenToonz Testing ya está en tu sistema y puedes
+abrirlo yendo a ${bold}Aplicaciones > Gráficos > OpenToonz${normal} o desde 
+la terminal con el comando ${bold}opentoonz${normal}
 "
 
 exit 0
